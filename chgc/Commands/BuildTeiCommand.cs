83ce88d734660a39f3d5c44afb6f65f8a165858e
Myrmex <dfusi@hotmail.com>
2023-06-26 14:10:27 +0200

    CLI

---
+++ chgc/Commands/BuildTeiCommand.cs
@@ -0,0 +1,85 @@
+﻿using Cadmus.Chgc.Cli.Services;
+using Cadmus.Chgc.Export;
+using Cadmus.Core;
+using Cadmus.Core.Storage;
+using Cadmus.Export;
+using Microsoft.Extensions.Configuration;
+using Spectre.Console;
+using Spectre.Console.Cli;
+using System.ComponentModel;
+
+namespace Cadmus.Chgc.Cli.Commands;
+
+internal sealed class BuildTeiCommand : AsyncCommand<BuildTeiCommandOptions>
+{
+    public override Task<int> ExecuteAsync(CommandContext context,
+        BuildTeiCommandOptions settings)
+    {
+        AnsiConsole.Status()
+            .Spinner(Spinner.Known.Star)
+            .Start("Building TEI...", ctx =>
+        {
+            // collector
+            AnsiConsole.MarkupLine("Creating collector...");
+            string cs = string.Format(
+                CliAppContext.Configuration.GetConnectionString("Default")!,
+                settings.DatabaseName);
+
+            MongoItemIdCollector collector = new();
+            collector.Configure(new MongoItemIdCollectorOptions
+            {
+                ConnectionString = cs,
+                GroupId = settings.GroupId,
+            });
+
+            // repository
+            AnsiConsole.MarkupLine("Creating repository...");
+            ICadmusRepository repository =
+                new ChgcRepositoryProvider().CreateRepository();
+
+            // composer
+            AnsiConsole.MarkupLine("Creating composer...");
+            FSChgcTeiItemComposer composer = new();
+            composer.Configure(new FSChgcTeiItemComposerOptions
+            {
+                OutputDirectory = settings.OutputDirectory
+            });
+
+            composer.Open();
+            foreach (string id in collector.GetIds())
+            {
+                IItem? item = repository.GetItem(id);
+                if (item == null) continue;
+
+                AnsiConsole.MarkupLine(item.ToString()!);
+                composer.Compose(item);
+            }
+            composer.Close();
+
+            ctx.Refresh();
+        });
+
+        return Task.FromResult(0);
+    }
+}
+
+public class BuildTeiCommandOptions : CommandSettings
+{
+    [CommandArgument(0, "<OutputDirectory>")]
+    [Description("The output directory")]
+    public string OutputDirectory { get; set; }
+
+    [CommandOption("-d|--database <DatabaseName>")]
+    [Description("The database name (default=cadmus-chgc)")]
+    [DefaultValue("cadmus-chgc")]
+    public string? DatabaseName { get; set; }
+
+    [CommandOption("-g|--group <GroupId>")]
+    [Description("The ID of the group to limit export to")]
+    public string? GroupId { get; set; }
+
+    public BuildTeiCommandOptions()
+    {
+        OutputDirectory = "";
+    }
+}
---
+++ chgc/Services/ChgcRepositoryProvider.cs
@@ -0,0 +1,70 @@
+﻿using Cadmus.Chgc.Parts;
+using Cadmus.Core;
+using Cadmus.Core.Config;
+using Cadmus.Core.Storage;
+using Cadmus.General.Parts;
+using Cadmus.Mongo;
+using System.Reflection;
+
+namespace Cadmus.Chgc.Cli.Services;
+
+internal class ChgcRepositoryProvider : IRepositoryProvider
+{
+    private readonly IPartTypeProvider _partTypeProvider;
+
+    /// <summary>
+    /// The connection string.
+    /// </summary>
+    public string ConnectionString { get; set; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="StandardRepositoryProvider"/>
+    /// class.
+    /// </summary>
+    /// <exception cref="ArgumentNullException">configuration</exception>
+    public ChgcRepositoryProvider()
+    {
+        ConnectionString = "";
+        TagAttributeToTypeMap map = new();
+        map.Add(new[]
+        {
+            // Cadmus.General.Parts
+            typeof(NotePart).GetTypeInfo().Assembly,
+            // Cadmus.Chgc.Parts
+            typeof(ChgcImageAnnotationsPart).GetTypeInfo().Assembly,
+        });
+
+        _partTypeProvider = new StandardPartTypeProvider(map);
+    }
+
+    /// <summary>
+    /// Gets the part type provider.
+    /// </summary>
+    /// <returns>part type provider</returns>
+    public IPartTypeProvider GetPartTypeProvider()
+    {
+        return _partTypeProvider;
+    }
+
+    /// <summary>
+    /// Creates a Cadmus repository.
+    /// </summary>
+    /// <returns>repository</returns>
+    /// <exception cref="ArgumentNullException">null database</exception>
+    public ICadmusRepository CreateRepository()
+    {
+        // create the repository (no need to use container here)
+        MongoCadmusRepository repository =
+            new(_partTypeProvider, new StandardItemSortKeyBuilder());
+
+        repository.Configure(new MongoCadmusRepositoryOptions
+        {
+            ConnectionString = ConnectionString ??
+            throw new InvalidOperationException(
+                "No connection string set for IRepositoryProvider implementation")
+        });
+
+        return repository;
+    }
+
+}
