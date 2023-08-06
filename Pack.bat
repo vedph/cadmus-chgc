a6bb5203de2b5d1a05a549b6baec5f7319d13e0d
Myrmex <dfusi@hotmail.com>
2023-08-06 12:59:10 +0200

    Batches

--- Pack.bat
+++ Pack.bat
@@ -3,6 +3,7 @@ echo BUILD Cadmus Chgc Packages
 del .\Cadmus.Chgc.Parts\bin\Debug\*.*nupkg
 del .\Cadmus.Chgc.Services\bin\Debug\*.*nupkg
 del .\Cadmus.Chgc.Export\bin\Debug\*.*nupkg
+del .\Cadmus.Chgc.Import\bin\Debug\*.*nupkg
 
 cd .\Cadmus.Chgc.Parts
 dotnet pack -c Debug -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
